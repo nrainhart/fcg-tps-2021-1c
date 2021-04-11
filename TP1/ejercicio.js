c8343ac6a71d15f0acc60e67df8b8e9e73ed20ae
Nico <nrainhart@10pines.com>
2021-04-11 13:07:43 -0300

    TP1 resuelto

--- TP1/ejercicio.js
+++ TP1/ejercicio.js
@@ -1,11 +1,72 @@
 // La imagen que tienen que modificar viene en el parámetro image y contiene inicialmente los datos originales
 // es objeto del tipo ImageData ( más info acá https://mzl.la/3rETTC6  )
 // Factor indica la cantidad de intensidades permitidas (sin contar el 0)
-function dither(image, factor) {
-    // completar
+
+const errorDiffusionFactorsFloyd = [
+    {displacement: {x: 1, y: 0}, factor: 7 / 16},
+    {displacement: {x: -1, y: 1}, factor: 3 / 16},
+    {displacement: {x: 0, y: 1}, factor: 5 / 16},
+    {displacement: {x: 1, y: 1}, factor: 1 / 16},
+];
+
+const errorDiffusionFactorsJarvis = [
+    {displacement: {x:1 , y:0 }, factor: 7/48},
+    {displacement: {x:2 , y:0 }, factor: 5/48},
+    {displacement: {x:-2 , y:1 }, factor: 3/48},
+    {displacement: {x:-1 , y:1 }, factor: 5/48},
+    {displacement: {x:0 , y:1 }, factor: 7/48},
+    {displacement: {x:1 , y:1 }, factor: 5/48},
+    {displacement: {x:2 , y:1 }, factor: 3/48},
+    {displacement: {x:-2 , y:2 }, factor: 1/48},
+    {displacement: {x:-1 , y:2 }, factor: 3/48},
+    {displacement: {x:0 , y:2 }, factor: 5/48},
+    {displacement: {x:1 , y:2 }, factor: 3/48},
+    {displacement: {x:2 , y:2 }, factor: 1/48},
+];
+
+function dither(image, factor, useJarvis) {
+    const factorNum = parseInt(factor);
+    const imageData = image.data;
+    const imageWidth = image.width;
+
+    const errorDiffusionFactors = useJarvis ? errorDiffusionFactorsJarvis : errorDiffusionFactorsFloyd;
+
+    for (let i = 0; i < imageData.length; i += 1) {
+        if(i%4 != 3){
+            var originalComponent = imageData[i];
+            var newComponent = quantizeComponentWithFactor(originalComponent, factorNum);
+            var err = originalComponent - newComponent;
+            imageData[i] = newComponent;
+            for(const errorDiffusionFactor of errorDiffusionFactors) {
+                const {x, y} = errorDiffusionFactor.displacement;
+                const linearCoord = i + x*4 + y * imageWidth * 4;
+                if (linearCoord < imageData.length) {
+                    imageData[linearCoord] += err * errorDiffusionFactor.factor;
+                }
+            }
+        }
+    }
+}
+
+function quantizeComponentWithFactor(oldComponent, factor){
+    var step = Math.floor(256/factor);
+    return Math.round(oldComponent/step) * step;
+}
+
+function componentDistance(comp1, comp2){
+    return Math.abs(comp1 - comp2);
 }
 
 // Imágenes a restar (imageA y imageB) y el retorno en result
 function substraction(imageA, imageB, result) {
-    // completar
+    const resultComponents = result.data;
+    const imageAComponents = imageA.data;
+    const imageBComponents = imageB.data;
+    for (let i = 0; i < resultComponents.length; i += 1) {
+        if(i%4 != 3){
+            resultComponents[i] = componentDistance(imageAComponents[i], imageBComponents[i]);
+        } else {
+            resultComponents[i] = 255;
+        }
+    }
 }
--- TP1/tp1.js
+++ TP1/tp1.js
@@ -1,96 +1,106 @@
 // Cargamos una imagen 
-function handleFiles(e) {
+function handleFiles(e)
+{
     // abrir el archivo
     console.log("> Cargando imagen...");
-    var URL = window.webkitURL || window.URL;
+    var URL      = window.webkitURL || window.URL;
     // canvas de entrada, de dithering y de diferencias
-    var canvasinput = document.getElementById('canvasinput');
-    var contextinput = canvasinput.getContext('2d');
-    var url = URL.createObjectURL(e.target.files[0]);
-    var img = new Image();
+    var canvasinput  = document.getElementById('canvasinput');
+    var contextinput  = canvasinput.getContext('2d');
+    var url      = URL.createObjectURL(e.target.files[0]);
+    var img      = new Image();
 
     // limpiamos el canvas
     contextinput.clearRect(0, 0, canvasinput.width, canvasinput.height);
 
     // dibujamos en el canvas (ojo que es asincrónica!)
-    img.onload = function () {
+    img.onload = function()
+    {
         // mostramos la imagen cargada
-        canvasinput.width = img.naturalWidth;
+        canvasinput.width  = img.naturalWidth;
         canvasinput.height = img.naturalHeight;
         contextinput.drawImage(img, 0, 0);
         console.log("> ... imagen cargada!");
 
+        var canvasresultFloyd  = document.getElementById('canvasresultFloyd');
+        var canvasdiffFloyd    = document.getElementById('canvasdiffFloyd');
+        var canvasresultJarvis  = document.getElementById('canvasresultJarvis');
+        var canvasdiffJarvis    = document.getElementById('canvasdiffJarvis');
+
         // computamos dither y diferencia
-        recomputeImages();
+        recomputeImages(canvasinput, canvasresultFloyd, canvasdiffFloyd, false);
+        recomputeImages(canvasinput, canvasresultJarvis, canvasdiffJarvis, true);
     }
 
     img.src = url;
 }
 
 // Computa las imágenes dereivadas (dither y substraction)
-function recomputeImages() {
+function recomputeImages(canvasinput, canvasresult, canvasdiff, useJarvis)
+{
     console.log("> Computando dithering y diferencias...");
     // obtenemos los canvas
-    var canvasinput = document.getElementById('canvasinput');
-    var canvasresult = document.getElementById('canvasresult');
-    var canvasdiff = document.getElementById('canvasdiff');
-    var contextinput = canvasinput.getContext('2d');
+    var contextinput  = canvasinput.getContext('2d');
     var contextresult = canvasresult.getContext('2d');
-    var contextdiff = canvasdiff.getContext('2d');
+    var contextdiff   = canvasdiff.getContext('2d');
 
-    // obtener niveles 
+    // obtener niveles
     var levels = document.getElementById('inputlevels').value;
 
     // limpiamos el canvas
     contextresult.clearRect(0, 0, canvasresult.width, canvasresult.height);
     contextdiff.clearRect(0, 0, canvasdiff.width, canvasdiff.height);
 
+    var inputImageData = contextinput.getImageData(0, 0, canvasinput.width, canvasinput.height);
+
     // filtramos la imagen
     // creamos la imagen nueva (slice copia los datos)
-    var ditherimg = new ImageData(contextinput.getImageData(0, 0, canvasinput.width, canvasinput.height).data,
-        canvasinput.width,
-        canvasinput.height);
+    var ditherimg = new ImageData(new Uint8ClampedArray(inputImageData.data), canvasinput.width, canvasinput.height);
 
     // filtrado
-    dither(ditherimg, levels)
+    dither(ditherimg,levels, useJarvis)
 
     // mostramos el resultado despuás del filtrado
-    canvasresult.width = canvasinput.width;
+    canvasresult.width  = canvasinput.width;
     canvasresult.height = canvasinput.height;
-    contextresult.putImageData(ditherimg, 0, 0);
+    contextresult.putImageData( ditherimg, 0, 0 );
     canvasresult.toDataURL('image/png');
 
     // diferencias
     // creamos la imagen nueva (slice copia los datos)
-    var subsimg = new ImageData(contextinput.getImageData(0, 0, canvasinput.width, canvasinput.height).data,
-        canvasinput.width,
-        canvasinput.height);
+    var subsimg = new ImageData(new Uint8ClampedArray(inputImageData.data), canvasinput.width, canvasinput.height);
 
     // filtrado
-    substraction(subsimg, ditherimg, subsimg)
+    substraction(inputImageData,ditherimg,subsimg);
 
     // mostramos el resultado despuás del filtrado
-    canvasdiff.width = canvasinput.width;
+    canvasdiff.width  = canvasinput.width;
     canvasdiff.height = canvasinput.height;
-    contextdiff.putImageData(subsimg, 0, 0);
+    contextdiff.putImageData( subsimg, 0, 0 );
     canvasdiff.toDataURL('image/png');
-
 
     console.log("> ... finalizado!");
 }
 
 // Inicialización del documento
-window.onload = function () {
+window.onload = function()
+{
     // asocio inputfile a la función addImage
-    var inputfile = document.getElementById('inputfile');
+    var inputfile   = document.getElementById('inputfile');
     inputfile.addEventListener('change', handleFiles);
 
     // asocio inputlevels a la función recomputeImages()
-    var inputlevels = document.getElementById('inputlevels');
-    inputlevels.addEventListener('change', recomputeImages);
+    var inputlevels   = document.getElementById('inputlevels');
+
+    var canvasresultFloyd  = document.getElementById('canvasresultFloyd');
+    var canvasdiffFloyd    = document.getElementById('canvasdiffFloyd');
+    var canvasresultJarvis  = document.getElementById('canvasresultJarvis');
+    var canvasdiffJarvis    = document.getElementById('canvasdiffJarvis');
+
+    inputlevels.addEventListener('change', () => {
+        recomputeImages(canvasinput, canvasresultFloyd, canvasdiffFloyd, false);
+        recomputeImages(canvasinput, canvasresultJarvis, canvasdiffJarvis, true);
+    });
 
     console.log("> HTML listo!");
 };
-
-
-

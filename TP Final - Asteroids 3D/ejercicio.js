d23972f9aa06fcdaee1f15190111b368a6fc52cf
Angel Yan <angelyan96@gmail.com>
2021-08-07 15:38:42 -0300

    Reformatear código

--- TP Final - Asteroids 3D/ejercicio.js
+++ TP Final - Asteroids 3D/ejercicio.js
@@ -3,153 +3,153 @@
 // transformaciones 3D (rotación, traslación y proyección) en una matriz de 4x4, representada por un arreglo en formato
 // column-major.
 function GetModelViewMatrix(translationX, translationY, translationZ, rotationX, rotationY) {
-	// Matriz de traslación
-	const trans = [
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		translationX, translationY, translationZ, 1
-	];
+    // Matriz de traslación
+    const trans = [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        translationX, translationY, translationZ, 1
+    ];
 
-	// Invertimos el signo de los ángulos para que el sentido de la rotación sea horario en vez de anti-horario
-	const cosRotX = Math.cos(-rotationX);
-	const sinRotX = Math.sin(-rotationX);
-	const rotX = [
-		1, 0, 0, 0,
-		0, cosRotX, -sinRotX, 0,
-		0, sinRotX, cosRotX, 0,
-		0, 0, 0, 1
-	];
-	const cosRotY = Math.cos(-rotationY);
-	const sinRotY = Math.sin(-rotationY);
-	const rotY = [
-		cosRotY, 0, sinRotY, 0,
-		0, 1, 0, 0,
-		-sinRotY, 0, cosRotY, 0,
-		0, 0, 0, 1
-	];
+    // Invertimos el signo de los ángulos para que el sentido de la rotación sea horario en vez de anti-horario
+    const cosRotX = Math.cos(-rotationX);
+    const sinRotX = Math.sin(-rotationX);
+    const rotX = [
+        1, 0, 0, 0,
+        0, cosRotX, -sinRotX, 0,
+        0, sinRotX, cosRotX, 0,
+        0, 0, 0, 1
+    ];
+    const cosRotY = Math.cos(-rotationY);
+    const sinRotY = Math.sin(-rotationY);
+    const rotY = [
+        cosRotY, 0, sinRotY, 0,
+        0, 1, 0, 0,
+        -sinRotY, 0, cosRotY, 0,
+        0, 0, 0, 1
+    ];
 
-	return [rotY, rotX, trans].reduce((firstTransform, secondTransform) => MatrixMult(secondTransform, firstTransform));
+    return [rotY, rotX, trans].reduce((firstTransform, secondTransform) => MatrixMult(secondTransform, firstTransform));
 }
 
 class MeshDrawer {
-	constructor(initialPosition) {
-		this.initialPosition = initialPosition;
+    constructor(initialPosition) {
+        this.initialPosition = initialPosition;
 
-		// 1. Compilamos el programa de shaders
-		this.prog = InitShaderProgram(meshVS, meshFS);
+        // 1. Compilamos el programa de shaders
+        this.prog = InitShaderProgram(meshVS, meshFS);
 
-		// 2. Obtenemos los IDs de las variables uniformes en los shaders
-		this.mvp = gl.getUniformLocation(this.prog, 'mvp');
-		this.mv = gl.getUniformLocation(this.prog, 'mv');
+        // 2. Obtenemos los IDs de las variables uniformes en los shaders
+        this.mvp = gl.getUniformLocation(this.prog, 'mvp');
+        this.mv = gl.getUniformLocation(this.prog, 'mv');
 
-		// 3. Obtenemos los IDs de los atributos de los vértices en los shaders
-		this.vertPos = gl.getAttribLocation(this.prog, 'pos');
-		this.normalPos = gl.getAttribLocation(this.prog, 'ncoord');
+        // 3. Obtenemos los IDs de los atributos de los vértices en los shaders
+        this.vertPos = gl.getAttribLocation(this.prog, 'pos');
+        this.normalPos = gl.getAttribLocation(this.prog, 'ncoord');
 
-		// 4. Creamos el buffer para los vertices
-		this.vertbuffer = gl.createBuffer();
-		this.normalbuffer = gl.createBuffer();
+        // 4. Creamos el buffer para los vertices
+        this.vertbuffer = gl.createBuffer();
+        this.normalbuffer = gl.createBuffer();
 
-		// 5. Texturas
-		this.texPosbuffer = gl.createBuffer();
-		this.textura = gl.createTexture();
+        // 5. Texturas
+        this.texPosbuffer = gl.createBuffer();
+        this.textura = gl.createTexture();
 
-		this.texGPULocation = gl.getUniformLocation(this.prog, 'texGPU');
-		this.useTexLocation = gl.getUniformLocation(this.prog, 'useTexture');
-		gl.useProgram(this.prog);
-		gl.uniform1i(this.useTexLocation, true);
+        this.texGPULocation = gl.getUniformLocation(this.prog, 'texGPU');
+        this.useTexLocation = gl.getUniformLocation(this.prog, 'useTexture');
+        gl.useProgram(this.prog);
+        gl.uniform1i(this.useTexLocation, true);
 
-		this.texCoordsLocation = gl.getAttribLocation(this.prog, 'tcoord');
+        this.texCoordsLocation = gl.getAttribLocation(this.prog, 'tcoord');
 
-		// 6. Iluminación
-		this.shininessLocation = gl.getUniformLocation(this.prog, 'shininess');
-		this.normalMatrixLocation = gl.getUniformLocation(this.prog, 'mn');
-		this.lightDirLocation = gl.getUniformLocation(this.prog, 'lightDir');
-	}
+        // 6. Iluminación
+        this.shininessLocation = gl.getUniformLocation(this.prog, 'shininess');
+        this.normalMatrixLocation = gl.getUniformLocation(this.prog, 'mn');
+        this.lightDirLocation = gl.getUniformLocation(this.prog, 'lightDir');
+    }
 
-	// Esta función se llama cada vez que el usuario carga un nuevo archivo OBJ. En los argumentos de esta función llegan
-	// un arreglo con las posiciones 3D de los vértices, un arreglo 2D con las  coordenadas de textura y las normales
-	// correspondientes a cada vértice. Todos los items en estos arreglos son del tipo float. Los vértices y normales se
-	// componen de a tres elementos consecutivos en el arreglo vertPos [x0,y0,z0,x1,y1,z1,..] y normals
-	// [n0,n0,n0,n1,n1,n1,...]. De manera similar, las coordenadas de textura se componen de a 2 elementos consecutivos y
-	// se  asocian a cada vértice en orden.
-	setMesh( vertPos, texCoords, normals ) {
-		this.numTriangles = vertPos.length / 3 / 3;
-		this._setBufferData(this.vertbuffer, vertPos);
-		this._setBufferData(this.texPosbuffer, texCoords);
-		this._setBufferData(this.normalbuffer, normals);
-	}
+    // Esta función se llama cada vez que el usuario carga un nuevo archivo OBJ. En los argumentos de esta función llegan
+    // un arreglo con las posiciones 3D de los vértices, un arreglo 2D con las  coordenadas de textura y las normales
+    // correspondientes a cada vértice. Todos los items en estos arreglos son del tipo float. Los vértices y normales se
+    // componen de a tres elementos consecutivos en el arreglo vertPos [x0,y0,z0,x1,y1,z1,..] y normals
+    // [n0,n0,n0,n1,n1,n1,...]. De manera similar, las coordenadas de textura se componen de a 2 elementos consecutivos y
+    // se  asocian a cada vértice en orden.
+    setMesh(vertPos, texCoords, normals) {
+        this.numTriangles = vertPos.length / 3 / 3;
+        this._setBufferData(this.vertbuffer, vertPos);
+        this._setBufferData(this.texPosbuffer, texCoords);
+        this._setBufferData(this.normalbuffer, normals);
+    }
 
-	_setBufferData(buffer, vertPos) {
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertPos), gl.STATIC_DRAW);
-	}
+    _setBufferData(buffer, vertPos) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertPos), gl.STATIC_DRAW);
+    }
 
-	// Esta función se llama para dibujar la malla de triángulos. El argumento es la matriz model-view-projection
-	// (matrixMVP), la matriz model-view (matrixMV) que es retornada por GetModelViewProjection y la matriz de
-	// transformación de las normales (matrixNormal) que es la inversa transpuesta de matrixMV
-	draw(matrixMVP, matrixMV, matrixNormal) {
-		// Seleccionamos el shader
-		gl.useProgram(this.prog);
+    // Esta función se llama para dibujar la malla de triángulos. El argumento es la matriz model-view-projection
+    // (matrixMVP), la matriz model-view (matrixMV) que es retornada por GetModelViewProjection y la matriz de
+    // transformación de las normales (matrixNormal) que es la inversa transpuesta de matrixMV
+    draw(matrixMVP, matrixMV, matrixNormal) {
+        // Seleccionamos el shader
+        gl.useProgram(this.prog);
 
-		// Seteamos matriz de transformación
-		gl.uniformMatrix4fv(this.mv, false, matrixMV);
-		gl.uniformMatrix4fv(this.mvp, false, matrixMVP);
-		gl.uniformMatrix3fv(this.normalMatrixLocation, false, matrixNormal);
+        // Seteamos matriz de transformación
+        gl.uniformMatrix4fv(this.mv, false, matrixMV);
+        gl.uniformMatrix4fv(this.mvp, false, matrixMVP);
+        gl.uniformMatrix3fv(this.normalMatrixLocation, false, matrixNormal);
 
-		// Seteamos atributos del vertex shader
-		this._setVertexAttributeFromBuffer(this.vertPos, this.vertbuffer, 3);
-		this._setVertexAttributeFromBuffer(this.texCoordsLocation, this.texPosbuffer, 2);
-		this._setVertexAttributeFromBuffer(this.normalPos, this.normalbuffer, 3);
+        // Seteamos atributos del vertex shader
+        this._setVertexAttributeFromBuffer(this.vertPos, this.vertbuffer, 3);
+        this._setVertexAttributeFromBuffer(this.texCoordsLocation, this.texPosbuffer, 2);
+        this._setVertexAttributeFromBuffer(this.normalPos, this.normalbuffer, 3);
 
-		// Dibujamos
-		gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles * 3);
-	}
+        // Dibujamos
+        gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles * 3);
+    }
 
-	_setVertexAttributeFromBuffer(vertexAttribute, buffer, elementsPerVertex) {
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-		gl.vertexAttribPointer(vertexAttribute, elementsPerVertex, gl.FLOAT, false, 0, 0);
-		gl.enableVertexAttribArray(vertexAttribute);
-	}
+    _setVertexAttributeFromBuffer(vertexAttribute, buffer, elementsPerVertex) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.vertexAttribPointer(vertexAttribute, elementsPerVertex, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vertexAttribute);
+    }
 
-	// Esta función se llama para setear una textura sobre la malla
-	// El argumento es un componente <img> de html que contiene la textura.
-	setTexture(img) {
-		gl.bindTexture( gl.TEXTURE_2D, this.textura);
-		gl.texImage2D( gl.TEXTURE_2D, // Textura 2D
-			0, // Mipmap nivel 0
-			gl.RGB, // formato (en GPU)
-			gl.RGB, // formato del input
-			gl.UNSIGNED_BYTE, // tipo
-			img // arreglo o <img>
-		);
-		gl.generateMipmap( gl.TEXTURE_2D );
+    // Esta función se llama para setear una textura sobre la malla
+    // El argumento es un componente <img> de html que contiene la textura.
+    setTexture(img) {
+        gl.bindTexture(gl.TEXTURE_2D, this.textura);
+        gl.texImage2D(gl.TEXTURE_2D, // Textura 2D
+            0, // Mipmap nivel 0
+            gl.RGB, // formato (en GPU)
+            gl.RGB, // formato del input
+            gl.UNSIGNED_BYTE, // tipo
+            img // arreglo o <img>
+        );
+        gl.generateMipmap(gl.TEXTURE_2D);
 
-		gl.activeTexture( gl.TEXTURE0 );
-		gl.bindTexture( gl.TEXTURE_2D, this.textura);
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this.textura);
 
-		gl.useProgram(this.prog);
-		gl.uniform1i (this.texGPULocation, 0 ); // Unidad 0
-	}
+        gl.useProgram(this.prog);
+        gl.uniform1i(this.texGPULocation, 0); // Unidad 0
+    }
 
-	// Esta función se llama cada vez que el usuario cambia el estado del checkbox 'Mostrar textura'
-	showTexture(show) {
-		gl.useProgram(this.prog);
-		gl.uniform1i(this.useTexLocation, show);
-	}
+    // Esta función se llama cada vez que el usuario cambia el estado del checkbox 'Mostrar textura'
+    showTexture(show) {
+        gl.useProgram(this.prog);
+        gl.uniform1i(this.useTexLocation, show);
+    }
 
-	// Este método se llama al actualizar la dirección de la luz desde la interfaz
-	setLightDir(x, y, z) {
-		gl.useProgram(this.prog);
-		gl.uniform3f(this.lightDirLocation, x, y, z);
-	}
+    // Este método se llama al actualizar la dirección de la luz desde la interfaz
+    setLightDir(x, y, z) {
+        gl.useProgram(this.prog);
+        gl.uniform3f(this.lightDirLocation, x, y, z);
+    }
 
-	// Este método se llama al actualizar el brillo del material
-	setShininess(shininess) {
-		gl.useProgram(this.prog);
-		gl.uniform1f(this.shininessLocation, shininess);
-	}
+    // Este método se llama al actualizar el brillo del material
+    setShininess(shininess) {
+        gl.useProgram(this.prog);
+        gl.uniform1f(this.shininessLocation, shininess);
+    }
 }
 
 var meshVS = `
--- TP Final - Asteroids 3D/lightview.js
+++ TP Final - Asteroids 3D/lightview.js
@@ -1,181 +1,171 @@
 // Controles para la dirección de la luz
 var lightView;
 
-class LightView
-{
-	constructor()
-	{
-		this.canvas = document.getElementById("lightcontrol");
-		this.canvas.oncontextmenu = function() {return false;};
+class LightView {
+    constructor() {
+        this.canvas = document.getElementById("lightcontrol");
+        this.canvas.oncontextmenu = function () {
+            return false;
+        };
 
-		// Inicializamos el canvas WebGL
-		this.gl = this.canvas.getContext("webgl", {antialias: false, depth: true});
+        // Inicializamos el canvas WebGL
+        this.gl = this.canvas.getContext("webgl", {antialias: false, depth: true});
 
-		if (!this.gl) 
-		{
-			alert("Imposible inicializar WebGL. Tu navegador quizás no lo soporte.");
-			return;
-		}
-		
-		// Inicialización
-		this.gl.clearColor(0.33,0.33,0.33,0);
-		this.gl.enable(gl.DEPTH_TEST);
-		
-		this.rotX = 0;
-		this.rotY = 0;
-		this.posZ = 5;
-		
-		this.resCircle = 32;
-		this.resArrow = 16;
-		this.buffer = this.gl.createBuffer();
-		var data = [];
+        if (!this.gl) {
+            alert("Imposible inicializar WebGL. Tu navegador quizás no lo soporte.");
+            return;
+        }
 
-		for ( var i=0; i<=this.resCircle; ++i ) 
-		{
-			var a = 2 * Math.PI * i / this.resCircle;
-			var x = Math.cos(a);
-			var y = Math.sin(a);
-			data.push( x * .9 );
-			data.push( y * .9 );
-			data.push( 0 );
-			data.push( x );
-			data.push( y );
-			data.push( 0 );
-		}
+        // Inicialización
+        this.gl.clearColor(0.33, 0.33, 0.33, 0);
+        this.gl.enable(gl.DEPTH_TEST);
 
-		for ( var i=0; i<=this.resCircle; ++i ) 
-		{
-			var a = 2 * Math.PI * i / this.resCircle;
-			var x = Math.cos(a);
-			var y = Math.sin(a);
-			data.push( x );
-			data.push( y );
-			data.push( -.05 );
-			data.push( x );
-			data.push( y );
-			data.push( 0.05 );
-		}
+        this.rotX = 0;
+        this.rotY = 0;
+        this.posZ = 5;
 
-		for ( var i=0; i<=this.resArrow; ++i ) 
-		{
-			var a = 2 * Math.PI * i / this.resArrow;
-			var x = Math.cos(a) * .07;
-			var y = Math.sin(a) * .07;
-			data.push( x );
-			data.push( y );
-			data.push( -1 );
-			data.push( x );
-			data.push( y );
-			data.push( 0 );
-		}
+        this.resCircle = 32;
+        this.resArrow = 16;
+        this.buffer = this.gl.createBuffer();
+        var data = [];
 
-		data.push( 0 );
-		data.push( 0 );
-		data.push( -1.2 );
+        for (var i = 0; i <= this.resCircle; ++i) {
+            var a = 2 * Math.PI * i / this.resCircle;
+            var x = Math.cos(a);
+            var y = Math.sin(a);
+            data.push(x * .9);
+            data.push(y * .9);
+            data.push(0);
+            data.push(x);
+            data.push(y);
+            data.push(0);
+        }
 
-		for ( var i=0; i<=this.resArrow; ++i ) 
-		{
-			var a = 2 * Math.PI * i / this.resArrow;
-			var x = Math.cos(a) * .15;
-			var y = Math.sin(a) * .15;
-			data.push( x );
-			data.push( y );
-			data.push( -0.9 );
-		}
+        for (var i = 0; i <= this.resCircle; ++i) {
+            var a = 2 * Math.PI * i / this.resCircle;
+            var x = Math.cos(a);
+            var y = Math.sin(a);
+            data.push(x);
+            data.push(y);
+            data.push(-.05);
+            data.push(x);
+            data.push(y);
+            data.push(0.05);
+        }
 
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
-		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(data), this.gl.STATIC_DRAW);
-		
-		//Viewport size
-		this.canvas.style.width  = "";
-		this.canvas.style.height = "";
+        for (var i = 0; i <= this.resArrow; ++i) {
+            var a = 2 * Math.PI * i / this.resArrow;
+            var x = Math.cos(a) * .07;
+            var y = Math.sin(a) * .07;
+            data.push(x);
+            data.push(y);
+            data.push(-1);
+            data.push(x);
+            data.push(y);
+            data.push(0);
+        }
 
-		const pixelRatio = window.devicePixelRatio || 1;
-		this.canvas.width  = pixelRatio * this.canvas.clientWidth;
-		this.canvas.height = pixelRatio * this.canvas.clientHeight;
+        data.push(0);
+        data.push(0);
+        data.push(-1.2);
 
-		const width  = (this.canvas.width  / pixelRatio);
-		const height = (this.canvas.height / pixelRatio);
+        for (var i = 0; i <= this.resArrow; ++i) {
+            var a = 2 * Math.PI * i / this.resArrow;
+            var x = Math.cos(a) * .15;
+            var y = Math.sin(a) * .15;
+            data.push(x);
+            data.push(y);
+            data.push(-0.9);
+        }
 
-		this.canvas.style.width  = width  + 'px';
-		this.canvas.style.height = height + 'px';
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(data), this.gl.STATIC_DRAW);
 
-		this.gl.viewport( 0, 0, this.canvas.width, this.canvas.height );
-		this.proj = ProjectionMatrix( this.canvas, this.posZ, 30 );
-		
-		// Compilamos los shaders
-		this.prog = InitShaderProgram( lightViewVS, lightViewFS, this.gl );
+        //Viewport size
+        this.canvas.style.width = "";
+        this.canvas.style.height = "";
 
-		this.mvp = this.gl.getUniformLocation( this.prog, 'mvp' );
-		this.clr1 = this.gl.getUniformLocation( this.prog, 'clr1' );
-		this.clr2 = this.gl.getUniformLocation( this.prog, 'clr2' );
-		this.vertPos = this.gl.getAttribLocation( this.prog, 'pos' );
-		
-		// Dibujamos
-		this.draw();
-		this.updateLightDir();
-		
-		// Evento de click
-		this.canvas.onmousedown = function() 
-		{
-			var cx = event.clientX;
-			var cy = event.clientY;
-			lightView.canvas.onmousemove = function() 
-			{
-				lightView.rotY += (cx - event.clientX)/lightView.canvas.width*5;
-				lightView.rotX += (cy - event.clientY)/lightView.canvas.height*5;
+        const pixelRatio = window.devicePixelRatio || 1;
+        this.canvas.width = pixelRatio * this.canvas.clientWidth;
+        this.canvas.height = pixelRatio * this.canvas.clientHeight;
 
-				cx = event.clientX;
-				cy = event.clientY;
+        const width = (this.canvas.width / pixelRatio);
+        const height = (this.canvas.height / pixelRatio);
 
-				lightView.draw();
-				lightView.updateLightDir();
-			}
-		}
-		
-		this.canvas.onmouseup = this.canvas.onmouseleave = function() 
-		{
-			lightView.canvas.onmousemove = null;
-		}
-	}
-	
-	updateLightDir()
-	{
-		var cy = Math.cos( this.rotY );
-		var sy = Math.sin( this.rotY );
-		var cx = Math.cos( this.rotX );
-		var sx = Math.sin( this.rotX );
-		meshDrawers.forEach(meshDrawer => meshDrawer.setLightDir( -sy, cy*sx, -cy*cx ));
-		DrawScene();
-	}
-	
-	draw()
-	{
-		// Limpiamos la pantalla
-		this.gl.clear( this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT );
-		
-		this.gl.bindBuffer( this.gl.ARRAY_BUFFER, this.buffer );
-		this.gl.vertexAttribPointer( this.vertPos, 3, this.gl.FLOAT, false, 0, 0 );
-		this.gl.enableVertexAttribArray( this.buffer );
+        this.canvas.style.width = width + 'px';
+        this.canvas.style.height = height + 'px';
 
-		this.gl.useProgram( this.prog );
-		var mvp = MatrixMult( this.proj, [ 1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,this.posZ,1 ] );
-		this.gl.uniformMatrix4fv( this.mvp, false, mvp );
-		this.gl.uniform3f( this.clr1, 0.6,0.6,0.6 );
-		this.gl.uniform3f( this.clr2, 0,0,0 );
-		this.gl.drawArrays( this.gl.TRIANGLE_STRIP, 0, this.resCircle*2+2 );
+        this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+        this.proj = ProjectionMatrix(this.canvas, this.posZ, 30);
 
-		var mv  = GetModelViewMatrix( 0, 0, this.posZ, this.rotX, this.rotY );
-		var mvp = MatrixMult( this.proj, mv );
-		this.gl.uniformMatrix4fv( this.mvp, false, mvp );
-		this.gl.uniform3f( this.clr1, 1,1,1 );
-		this.gl.drawArrays( this.gl.TRIANGLE_STRIP, 0, this.resCircle*2+2 );
-		this.gl.drawArrays( this.gl.TRIANGLE_STRIP, this.resCircle*2+2, this.resCircle*2+2 );
-		this.gl.uniform3f( this.clr1, 0,0,0 );
-		this.gl.uniform3f( this.clr2, 1,1,1 );
-		this.gl.drawArrays( this.gl.TRIANGLE_STRIP, this.resCircle*4+4, this.resArrow*2+2 );
-		this.gl.drawArrays( this.gl.TRIANGLE_FAN, this.resCircle*4+4 + this.resArrow*2+2, this.resArrow+2 );
-	}
+        // Compilamos los shaders
+        this.prog = InitShaderProgram(lightViewVS, lightViewFS, this.gl);
+
+        this.mvp = this.gl.getUniformLocation(this.prog, 'mvp');
+        this.clr1 = this.gl.getUniformLocation(this.prog, 'clr1');
+        this.clr2 = this.gl.getUniformLocation(this.prog, 'clr2');
+        this.vertPos = this.gl.getAttribLocation(this.prog, 'pos');
+
+        // Dibujamos
+        this.draw();
+        this.updateLightDir();
+
+        // Evento de click
+        this.canvas.onmousedown = function () {
+            var cx = event.clientX;
+            var cy = event.clientY;
+            lightView.canvas.onmousemove = function () {
+                lightView.rotY += (cx - event.clientX) / lightView.canvas.width * 5;
+                lightView.rotX += (cy - event.clientY) / lightView.canvas.height * 5;
+
+                cx = event.clientX;
+                cy = event.clientY;
+
+                lightView.draw();
+                lightView.updateLightDir();
+            }
+        }
+
+        this.canvas.onmouseup = this.canvas.onmouseleave = function () {
+            lightView.canvas.onmousemove = null;
+        }
+    }
+
+    updateLightDir() {
+        var cy = Math.cos(this.rotY);
+        var sy = Math.sin(this.rotY);
+        var cx = Math.cos(this.rotX);
+        var sx = Math.sin(this.rotX);
+        meshDrawers.forEach(meshDrawer => meshDrawer.setLightDir(-sy, cy * sx, -cy * cx));
+        DrawScene();
+    }
+
+    draw() {
+        // Limpiamos la pantalla
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
+        this.gl.vertexAttribPointer(this.vertPos, 3, this.gl.FLOAT, false, 0, 0);
+        this.gl.enableVertexAttribArray(this.buffer);
+
+        this.gl.useProgram(this.prog);
+        var mvp = MatrixMult(this.proj, [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, this.posZ, 1]);
+        this.gl.uniformMatrix4fv(this.mvp, false, mvp);
+        this.gl.uniform3f(this.clr1, 0.6, 0.6, 0.6);
+        this.gl.uniform3f(this.clr2, 0, 0, 0);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, this.resCircle * 2 + 2);
+
+        var mv = GetModelViewMatrix(0, 0, this.posZ, this.rotX, this.rotY);
+        var mvp = MatrixMult(this.proj, mv);
+        this.gl.uniformMatrix4fv(this.mvp, false, mvp);
+        this.gl.uniform3f(this.clr1, 1, 1, 1);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, this.resCircle * 2 + 2);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, this.resCircle * 2 + 2, this.resCircle * 2 + 2);
+        this.gl.uniform3f(this.clr1, 0, 0, 0);
+        this.gl.uniform3f(this.clr2, 1, 1, 1);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, this.resCircle * 4 + 4, this.resArrow * 2 + 2);
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, this.resCircle * 4 + 4 + this.resArrow * 2 + 2, this.resArrow + 2);
+    }
 }
 
 // Vertex shader
